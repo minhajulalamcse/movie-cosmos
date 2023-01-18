ad6d34c0f3ae3e5079716ea503acc8f33cd29a47
Minhajul Alam <minhajulalam.cse@gmail.com>
2023-01-18 06:34:22 +0600

    feat: add theme file

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
+import { ThemeProvider } from '@mui/material'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
-
+import { theme } from './theme'
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 )
---
+++ src/theme.ts
@@ -0,0 +1,21 @@
+import { createTheme } from '@mui/material'
+export const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#13315c',
+      light: '#134074',
+      dark: '#0b2545',
+      contrastText: '#ffffff'
+    },
+    secondary: {
+      main: '#f6c71a',
+      light: '#fed230',
+      dark: '#d9ab00',
+      contrastText: '#000000'
+    }
+  },
+  typography: {
+    fontFamily: "'Inter',sans-serif"
+  }
+})
