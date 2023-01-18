e44d945fd1ac4966065129af056cbefaba7f0b89
Minhajul Alam <minhajulalam.cse@gmail.com>
2023-01-19 04:19:52 +0600

    fix: typo

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Box, CssBaseline, Toolbar } from '@mui/material'
 import React, { FC } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { AppbarWithDrawer } from './components'
-import { Homepage, MovieDetailsPage } from './pages'
+import { HomePage, MovieDetailsPage } from './pages'
 
 const App: FC = () => {
   return (
@@ -12,7 +12,7 @@ const App: FC = () => {
       <Toolbar />
       <Box component='main'>
         <Routes>
-          <Route path='/' element={<Homepage />} />
+          <Route path='/' element={<HomePage />} />
           <Route path='/movie/:id' element={<MovieDetailsPage />} />
         </Routes>
       </Box>
