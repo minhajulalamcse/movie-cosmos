9576d8ab33035654c93e38ab5cae7142ed654a51
Minhajul Alam <minhajulalam.cse@gmail.com>
2023-01-19 06:02:25 +0600

    feat: movie details page component implement with api data

--- src/components/MovieInformation/MovieInformation.tsx
+++ src/components/MovieInformation/MovieInformation.tsx
@@ -5,12 +5,14 @@ import {
   Movie as MovieIcon,
   WatchLater as WatchLaterIcon
 } from '@mui/icons-material'
-import { alpha, Box, IconButton, Rating, styled, Typography, useTheme } from '@mui/material'
+import { alpha, Box, Card, CardMedia, IconButton, Rating, styled, Typography, useTheme } from '@mui/material'
 import React, { FC } from 'react'
 import { useNavigate } from 'react-router-dom'
 import ShowMoreText from 'react-show-more-text'
+import { BlackImage } from '../../assets'
 import { IMovieDetailsGetResponse } from '../../interfaces/movies/IMovieDetailsGetResponse'
-import { getYearFromDate, toHoursAndMinutes } from '../../utils'
+import { getFormattedDate, getYearFromDate, toHoursAndMinutes } from '../../utils'
+import { GlassEffect } from '../Common/GlassEffect/GlassEffect'
 
 interface IMovieInformation {
   movie: IMovieDetailsGetResponse
@@ -18,7 +20,7 @@ interface IMovieInformation {
 export const MovieInformation: FC<IMovieInformation> = ({ movie }) => {
   const theme = useTheme()
   const navigate = useNavigate()
-
+  console.log(movie)
   const TopSectionInfoWrapper = styled(Box)(({ theme }) => ({
     position: 'absolute',
     top: 0,
@@ -36,15 +38,18 @@ export const MovieInformation: FC<IMovieInformation> = ({ movie }) => {
     position: 'absolute',
     bottom: 0,
     left: 0,
+    right: 0,
     padding: '32px',
     color: theme.palette.common.white,
     background: `linear-gradient(0,rgba(0, 0, 0, 1), rgba(0, 0, 0, 0) 100%
     )`,
-    width: '100%'
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-end'
   }))
 
   const OverviewText = styled(Typography)(({ theme }) => ({
-    width: '70%',
+    width: '100%',
     lineHeight: '20px',
     [theme.breakpoints.down('md')]: {
       width: '100%'
@@ -95,74 +100,117 @@ export const MovieInformation: FC<IMovieInformation> = ({ movie }) => {
         </Box>
       </TopSectionInfoWrapper>
       <BottomSectionInfoWrapper>
-        <Typography variant='h4' fontWeight={theme.typography.fontWeightBold} color='inherit'>
-          {movie?.title}
-        </Typography>
-        <Typography
-          variant='h6'
-          fontWeight={theme.typography.fontWeightMedium}
-          gutterBottom
-          color={alpha('#ffffff', 0.9)}
+        <Card
+          sx={{
+            width: '400px',
+            height: '450px',
+            boxShadow: 3,
+            borderRadius: 2,
+            position: 'relative',
+            display: { xs: 'none', md: 'block' }
+          }}
         >
-          {movie?.tagline}
-        </Typography>
-        <OverviewText
-          variant='subtitle1'
-          fontWeight={theme.typography.fontWeightMedium}
-          letterSpacing={0.5}
-          color={alpha('#ffffff', 0.8)}
-        >
-          <ShowMoreText
-            lines={1}
-            more='Show more'
-            less='Show less'
-            className='content-css'
-            anchorClass='show-more-less-clickable'
-            expanded={false}
-            truncatedEndingComponent={'... '}
+          <CardMedia
+            sx={{
+              width: '100%',
+              height: '100%',
+              backgroundColor: '#000'
+            }}
+            image={movie?.poster_path !== null ? `https://image.tmdb.org/t/p/w500/${movie?.poster_path}` : BlackImage}
+            title={movie?.title}
           >
-            {movie?.overview}
-          </ShowMoreText>
-        </OverviewText>
+            <GlassEffect />
+          </CardMedia>
+        </Card>
         <Box
+          ml={2}
           display='flex'
+          flexDirection='column'
+          justifyContent='flex-end'
           alignItems='flex-start'
-          justifyContent='space-between'
-          color={alpha('#ffffff', 0.8)}
-          mt={1}
-          sx={{ flexDirection: { xs: 'column', md: 'row' } }}
+          width='100%'
         >
+          <Typography variant='h4' fontWeight={theme.typography.fontWeightBold} color='inherit'>
+            {movie?.title}{' '}
+            <Typography
+              variant='h4'
+              fontWeight={theme.typography.fontWeightRegular}
+              color={alpha('#ffffff', 0.8)}
+              display='inline-block'
+            >
+              ({getYearFromDate(movie?.release_date)})
+            </Typography>
+          </Typography>
           <Box display='flex' alignItems='center' gap='10px' mt={1}>
             <Typography color='inherit' fontWeight={theme.typography.fontWeightMedium}>
-              {getYearFromDate(movie?.release_date)}
+              {getFormattedDate(movie?.release_date)}
             </Typography>
             &bull;
             <Typography color='inherit' fontWeight={theme.typography.fontWeightMedium}>
               {toHoursAndMinutes(movie?.runtime)}
             </Typography>
           </Box>
-          <Box mt={1}>
-            <Box display='flex' flexDirection='column' justifyContent='center' alignItems='flex-start'>
-              <Box display='flex' alignItems='center' gap={'10px'}>
-                <Rating
-                  size='small'
-                  readOnly
-                  value={movie.vote_average / 2}
-                  precision={0.1}
-                  sx={{
-                    '.MuiRating-icon.MuiRating-iconEmpty': {
-                      color: '#ffffff50'
-                    }
-                  }}
-                />
-                <Typography color='inherit' variant='body2' fontWeight={theme.typography.fontWeightMedium}>
-                  {(movie?.vote_average).toPrecision(2)} / 10
-                </Typography>
-              </Box>
-              <Box display='flex' alignItems='center' gap={'10px'}>
-                <Typography color='inherit' variant='body2' fontWeight={theme.typography.fontWeightMedium}>
-                  {movie?.vote_count} reviews
-                </Typography>
+          <Typography
+            variant='subtitle1'
+            fontWeight={theme.typography.fontWeightRegular}
+            fontStyle='italic'
+            gutterBottom
+            color={alpha('#ffffff', 0.9)}
+          >
+            {movie?.tagline}
+          </Typography>
+          <Typography variant='h6' fontWeight={theme.typography.fontWeightMedium} gutterBottom color='inherit'>
+            Overview
+          </Typography>
+          <OverviewText
+            variant='body2'
+            fontWeight={theme.typography.fontWeightMedium}
+            letterSpacing={0.5}
+            color={alpha('#ffffff', 0.8)}
+          >
+            <ShowMoreText
+              lines={1}
+              more='Show more'
+              less='Show less'
+              className='content-css'
+              anchorClass='show-more-less-clickable'
+              expanded={false}
+              truncatedEndingComponent={'... '}
+            >
+              {movie?.overview}
+            </ShowMoreText>
+          </OverviewText>
+          <Box
+            display='flex'
+            alignItems='flex-start'
+            justifyContent='space-between'
+            color={alpha('#ffffff', 0.8)}
+            mt={1}
+            sx={{ flexDirection: { xs: 'column', md: 'row' } }}
+          >
+            <Box mt={1}>
+              <Box display='flex' flexDirection='column' justifyContent='center' alignItems='flex-start'>
+                <Box display='flex' alignItems='center' gap={'10px'}>
+                  <Rating
+                    size='small'
+                    readOnly
+                    value={movie.vote_average / 2}
+                    precision={0.1}
+                    sx={{
+                      '.MuiRating-icon.MuiRating-iconEmpty': {
+                        color: '#ffffff50'
+                      }
+                    }}
+                  />
+                  <Typography color='inherit' variant='body2' fontWeight={theme.typography.fontWeightMedium}>
+                    {(movie?.vote_average).toPrecision(2)} / 10
+                  </Typography>
+                </Box>
+                <Box display='flex' alignItems='center' gap={'10px'}>
+                  <Typography color='inherit' variant='body2' fontWeight={theme.typography.fontWeightMedium}>
+                    {movie?.vote_count} reviews
+                  </Typography>
+                </Box>
               </Box>
             </Box>
           </Box>
