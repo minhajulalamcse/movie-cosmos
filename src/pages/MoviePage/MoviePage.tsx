b4aeef0dc19fe0eee1e8c68b5e7123f554831a36
Minhajul Alam <minhajulalam.cse@gmail.com>
2023-01-20 02:17:38 +0600

    feat: get popular movies api call implement with UI

--- src/pages/MoviePage/MoviePage.tsx
+++ src/pages/MoviePage/MoviePage.tsx
@@ -1,16 +1,43 @@
 import { Container, Grid } from '@mui/material'
 import { FC } from 'react'
+import { CardWithGlassEffect, Error, Loading, NoDataFound } from '../../components'
+import { IMovieDetailsGetResponse } from '../../interfaces/movies/IMovieDetailsGetResponse'
+import { useGetPopularMoviesQuery } from '../../services/tmdb'
 
 export const MoviePage: FC = () => {
+  const { data, isLoading, isError } = useGetPopularMoviesQuery(null)
+
+  if (isError) {
+    return <Error />
+  }
+  if (isLoading) {
+    return <Loading />
+  }
+  if (data === null || data === undefined) {
+    return <NoDataFound />
+  }
   return (
-    <Container>
+    <Container sx={{ margin: '24px auto' }}>
       <Grid container spacing={3}>
         <Grid item xs={12} lg={3}>
           Filters
         </Grid>
         <Grid item xs={12} lg={9}>
           <Grid container spacing={3}>
-            <Grid item xs={12} sm={2} md={3} lg={4}></Grid>
+            {data?.results.map((movie: IMovieDetailsGetResponse, index: number) => {
+              return (
+                <Grid item xs={12} sm={4} md={3} key={index}>
+                  <CardWithGlassEffect
+                    imagePath={movie?.poster_path}
+                    link={`/movie/${movie.id}`}
+                    glassEffect={true}
+                    title={movie?.title}
+                    voteAverage={movie?.vote_average}
+                    subTitle={movie?.release_date}
+                  />
+                </Grid>
+              )
+            })}
           </Grid>
         </Grid>
       </Grid>
