ae3dbd697bd939bb476aa8dbdcdf5ec51b60e19a
Minhajul Alam <minhajulalam.cse@gmail.com>
2023-01-19 09:34:16 +0600

    feat: add poster and backdrops in movie details page

--- src/components/MovieMediaSection/MovieMediaSection.tsx
+++ src/components/MovieMediaSection/MovieMediaSection.tsx
@@ -1,6 +1,9 @@
-import { Box, Tab, Tabs } from '@mui/material'
+import { Box, Tab, Tabs, Typography, useTheme } from '@mui/material'
 import { FC, useState } from 'react'
-import { MovieVideos } from '../MovieVideos/MovieVideos'
+import { useNavigate, useParams } from 'react-router-dom'
+import { Error, Loading, MoviePosterBackdropsTab } from '..'
+import { useGetMovieImagesQuery } from '../../services/tmdb'
+import { NoDataFound } from '../Common/NoDataFound/NoDataFound'
 
 interface ITabPanelProps {
   children?: React.ReactNode
@@ -27,13 +30,32 @@ const TabPanel: FC<ITabPanelProps> = (props: ITabPanelProps) => {
 
 export const MovieMediaSection: FC = () => {
   const [value, setValue] = useState(0)
+  const theme = useTheme()
+  const { id } = useParams()
+  const navigate = useNavigate()
 
+  if ((typeof id === 'string' && Number.isNaN(parseInt(id))) || id === undefined) {
+    navigate('/')
+  }
+  const { data, isLoading, isError } = useGetMovieImagesQuery(Number(id))
+  if (isError) {
+    return <Error />
+  }
+  if (isLoading) {
+    return <Loading />
+  }
+  if (data === null || data === undefined || (data?.backdrops.length === 0 && data?.posters.length === 0)) {
+    return <NoDataFound />
+  }
   const handleChange = (event: React.SyntheticEvent, newValue: number): void => {
     setValue(newValue)
   }
 
   return (
-    <Box m={2}>
+    <Box display='flex' flexDirection='column' alignItems='flex-start' justifyContent='flex-start' my={2} mr={0}>
+      <Typography variant='h5' fontWeight={theme.typography.fontWeightMedium} ml='10px'>
+        Media
+      </Typography>
       <Tabs
         value={value}
         onChange={handleChange}
@@ -41,18 +63,14 @@ export const MovieMediaSection: FC = () => {
         scrollButtons='auto'
         aria-label='scrollable tabs'
       >
-        <Tab label='Videos' />
         <Tab label='Posters' />
         <Tab label='Backdrops' />
       </Tabs>
       <TabPanel value={value} index={0}>
-        <MovieVideos />
+        <MoviePosterBackdropsTab images={data?.posters} columnCount={5} imageCount={9}/>
       </TabPanel>
       <TabPanel value={value} index={1}>
-        2
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        3
+        <MoviePosterBackdropsTab images={data?.backdrops} columnCount={3} imageCount={8}/>
       </TabPanel>
     </Box>
   )
